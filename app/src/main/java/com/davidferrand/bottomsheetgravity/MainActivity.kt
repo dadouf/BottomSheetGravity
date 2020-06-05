97fe8ed8a55c39edcf305105934a15f034daca68
David Ferrand <d.ferrand@emogi.com>
2020-06-05 11:25:36 +0200

    Pin view to bottom and simplify calculation

--- app/src/main/java/com/davidferrand/bottomsheetgravity/MainActivity.kt
+++ app/src/main/java/com/davidferrand/bottomsheetgravity/MainActivity.kt
@@ -14,22 +14,16 @@ class MainActivity : AppCompatActivity() {
         BottomSheetBehavior.from(bottom_sheet).apply {
             isHideable = true
             state = BottomSheetBehavior.STATE_HIDDEN
-            peekHeight = 600 // TODO do it for auto = 16:9 keyline
-
-            // TODO onLayout
 
             addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
                 override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                    if (slideOffset <= 0f) {
-                        // BottomSheet is between HIDDEN (visibleHeight=0) and COLLAPSED (visibleHeight=peekHeight)
-                        pinned_center.translationY =
-                            (slideOffset + 1) * (peekHeight - pinned_center.height) / 2
-                    } else {
-                        // BottomSheet is between COLLAPSED (visibleHeight=peekHeight) and EXPANDED (visibleHeight=parent.height)
-                        pinned_center.translationY =
-                            (1 - slideOffset) * (peekHeight - pinned_center.height) / 2 +
-                                    slideOffset * (bottomSheet.height - pinned_center.height) / 2
-                    }
+                    val bottomSheetVisibleHeight = bottomSheet.height - bottomSheet.top
+
+                    pinned_center.translationY =
+                        (bottomSheetVisibleHeight - pinned_center.height) / 2f
+
+                    pinned_bottom.translationY =
+                        (bottomSheetVisibleHeight - pinned_bottom.height).toFloat()
                 }
 
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
